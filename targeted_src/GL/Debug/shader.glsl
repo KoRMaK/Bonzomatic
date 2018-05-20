d773a1abf626fe7f1c34da509510323ac3bc3cd6
Corey Fleming <coreypfleming@gmail.com>
2018-05-20 18:31:56 -0400

    Stash shader

--- targeted_src/GL/Debug/shader.glsl
+++ targeted_src/GL/Debug/shader.glsl
@@ -7,6 +7,7 @@ uniform sampler1D texFFT; // towards 0.0 is bass / lower freq, towards 1.0 is hi
 uniform sampler1D texFFTSmoothed; // this one has longer falloff and less harsh transients
 uniform sampler1D texFFTIntegrated; // this is continually increasing
 uniform sampler2D texChecker;
+uniform sampler2D texFuckedUp;
 uniform sampler2D texNoise;
 uniform sampler2D texTex1;
 uniform sampler2D texTex2;
@@ -21,22 +22,41 @@ vec4 plas( vec2 v, float time )
   return vec4( sin(c * 0.2 + cos(time)), c * 0.15, cos( c * 0.1 + time / .4 ) * .25, 1.0 );
 }
 
+float wrap_up(float value, float max)
+{
+  if(value > max)
+  {
+    value = -0.5 + (value - 1.0);
+  }
+  return value;
+}
+
 void main(void)
 {
-  vec2 uv = vec2(gl_FragCoord.x / v2Resolution.x, gl_FragCoord.y / v2Resolution.y);
+  vec2 uv = vec2((gl_FragCoord.x) / v2Resolution.x, gl_FragCoord.y / v2Resolution.y);
+  //uv = vec2(0, 2.9);
+  //uv.y *= -1;
+  //uv *= -1; //whoa this makes a happy accident
   uv -= 0.5;
+  //uv.y -= 0.5;
+  //uv.x -= 0.7;
   uv /= vec2(v2Resolution.y / v2Resolution.x, 1);
+  //uv.x -= 0.1; 
 
-  vec2 tunnel_transformed_uv;
-  tunnel_transformed_uv.x = atan(uv.x / uv.y) / 3.14;
-  tunnel_transformed_uv.y = 1 / length(uv) * 0.2;
+  vec2 tunnel_transformed_uv = uv;
+  tunnel_transformed_uv.x = (atan(wrap_up(uv.x + 0.00, 1) / wrap_up(uv.y + 0, 1)) / 3.14);
+  tunnel_transformed_uv.y = ((0.2 / length(uv)) * 1);
+  tunnel_transformed_uv.y *= -1;
+  tunnel_transformed_uv.x *= -1;
+
 
   //float f = texture( texChecker, _d ).t;// * 100;
 
-  //m.x += sin( fGlobalTime ) * 0.1;
-  tunnel_transformed_uv.y += fGlobalTime * 0.05; //rate at which the thing moves forward
+  //tunnel_transformed_uv.x += sin( fGlobalTime ) * 0.1;
+  tunnel_transformed_uv.y -= fGlobalTime * 0.15; //rate at which the thing moves forward
 
-  vec4 _tex_info = texture(texTex3, tunnel_transformed_uv);
+  tunnel_transformed_uv.x += 0.5;
+  vec4 _tex_info = texture(texFuckedUp, tunnel_transformed_uv);
   //vec4 _t = plas( _m * 3.14, fGlobalTime ) / d;
   //_t = clamp( t, 0.0, 1.0 );
   out_color = _tex_info; // + _t; // + f 
